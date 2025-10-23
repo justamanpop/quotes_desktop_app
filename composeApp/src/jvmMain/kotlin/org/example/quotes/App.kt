69164ec31386dba2fd62833a369b622ba14a87ec
anish <pogchampoo24@gmail.com>
2025-10-23 22:31:15 +0530

    added search bar on top

--- composeApp/src/jvmMain/kotlin/org/example/quotes/App.kt
+++ composeApp/src/jvmMain/kotlin/org/example/quotes/App.kt
@@ -1,9 +1,6 @@
 package org.example.quotes
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,57 +8,66 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import quotes.composeapp.generated.resources.Res
-import quotes.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 
 @Composable
 @Preview
 fun App() {
+    var searchText = remember { mutableStateOf("") }
     MaterialTheme {
-        Box(modifier = Modifier.fillMaxSize().padding(12.dp, 12.dp, 12.dp, 12.dp)) {
-            val state = rememberLazyListState()
-            LazyColumn(
-                modifier = Modifier.fillMaxSize()
-                    .border(1.dp, DividerDefaults.color), state
-            ) {
-                fakeQuotes().forEach { quote ->
-                    item {
-                        Text(quote.first, fontSize = 24.sp, lineHeight = 32.sp, modifier = Modifier.padding(8.dp))
-                        Text(
-                            quote.second,
-                            fontSize = 12.sp,
-                            color = Color.Gray,
-                            modifier = Modifier.padding(start = 8.dp)
-                        )
-                        HorizontalDivider()
+        Column(modifier = Modifier.fillMaxSize()) {
+            TextField(
+                value = searchText.value,
+                onValueChange = { v ->
+                    searchText.value = v
+                },
+                placeholder = {Text("Search")},
+                leadingIcon = { Icon(Icons.Default.Search, "search") },
+                modifier = Modifier.padding(12.dp, 12.dp, 12.dp, 0.dp).width(600.dp)
+            )
+            Box(modifier = Modifier.fillMaxSize().padding(12.dp, 12.dp, 12.dp, 12.dp)) {
+                val state = rememberLazyListState()
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                        .border(1.dp, DividerDefaults.color), state
+                ) {
+                    fakeQuotes().forEach { quote ->
+                        item {
+                            Text(quote.first, fontSize = 24.sp, lineHeight = 32.sp, modifier = Modifier.padding(8.dp))
+                            Text(
+                                quote.second,
+                                fontSize = 12.sp,
+                                color = Color.Gray,
+                                modifier = Modifier.padding(start = 8.dp)
+                            )
+                            HorizontalDivider()
+                        }
                     }
                 }
+                VerticalScrollbar(
+                    modifier = Modifier.fillMaxHeight().align(Alignment.CenterEnd),
+                    adapter = rememberScrollbarAdapter(state)
+                )
             }
-            VerticalScrollbar(
-                modifier = Modifier.fillMaxHeight().align(Alignment.CenterEnd),
-                adapter = rememberScrollbarAdapter(state)
-            )
         }
+
     }
 }
