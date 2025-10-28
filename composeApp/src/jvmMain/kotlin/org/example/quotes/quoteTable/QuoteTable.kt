03ef94088f9432f9f6cde20003b70fb3b143c9e0
anish <pogchampoo24@gmail.com>
2025-10-28 09:23:56 +0530

    added copy functionality when clicking copy button

--- composeApp/src/jvmMain/kotlin/org/example/quotes/quoteTable/QuoteTable.kt
+++ composeApp/src/jvmMain/kotlin/org/example/quotes/quoteTable/QuoteTable.kt
@@ -18,18 +18,23 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.platform.ClipEntry
 import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
+import java.awt.datatransfer.Transferable
 
 @Composable
 fun QuoteTable(quotes: List<Quote>, modifier: Modifier = Modifier) {
-    val clipboardManager = LocalClipboard.current;
     Box(modifier = modifier) {
         val state = rememberLazyListState()
 
@@ -49,9 +54,17 @@ fun QuoteTable(quotes: List<Quote>, modifier: Modifier = Modifier) {
                                 modifier = Modifier.padding(start = 8.dp)
                             )
                         }
-                        Button(onClick = {}, modifier = Modifier.weight(1f).padding(top = 8.dp, end = 16.dp).width(2.dp).pointerHoverIcon(
-                            PointerIcon.Hand
-                        )) {
+                        Button(
+                            onClick = {
+                                val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+                                val stringSelection = StringSelection(quote.content)
+                                clipboard.setContents(stringSelection, null)
+                            },
+                            modifier = Modifier.weight(1f).padding(top = 8.dp, end = 16.dp).width(2.dp)
+                                .pointerHoverIcon(
+                                    PointerIcon.Hand
+                                )
+                        ) {
                             Icon(Icons.Default.ContentCopy, contentDescription = "copy")
                         }
                     }
