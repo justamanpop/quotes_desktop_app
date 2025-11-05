e351595959ffa912ec48b6b689bb177a8ca085e1
anish <pogchampoo24@gmail.com>
2025-11-05 19:52:58 +0530

    Added small x to selected tags

--- composeApp/src/jvmMain/kotlin/org/example/quotes/shared/SelectedTags.kt
+++ composeApp/src/jvmMain/kotlin/org/example/quotes/shared/SelectedTags.kt
@@ -3,6 +3,7 @@ package org.example.quotes.shared
 import Tag
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
@@ -13,12 +14,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SelectedTags(tags: Set<Tag>, removeTag: (Tag) -> Unit, modifier: Modifier = Modifier) {
@@ -27,20 +30,28 @@ fun SelectedTags(tags: Set<Tag>, removeTag: (Tag) -> Unit, modifier: Modifier = 
             var isTagFocused by remember { mutableStateOf(false) }
             Card(
                 colors = CardColors(
-                    containerColor = if (isTagFocused) Color.Gray else Color.LightGray,
+                    containerColor = if (isTagFocused) Color(185, 185, 199, 255) else Color.LightGray,
                     contentColor = Color(64, 126, 201),
                     disabledContainerColor = Color.LightGray,
                     disabledContentColor = Color.White,
                 ),
                 modifier = Modifier.padding(start = 8.dp)
-                    .pointerHoverIcon(PointerIcon.Hand)
                     .onFocusChanged { focusState -> isTagFocused = focusState.isFocused }
+                    .pointerHoverIcon(PointerIcon.Hand)
                     .lightBorderIfFocused(isTagFocused, 2.dp)
-                    .clickable(enabled = true, onClick = {
-                        removeTag(tag)
-                    })
+                    .clickable { removeTag(tag) }
             ) {
-                Text(tag.name, modifier = Modifier.padding(8.dp))
+                Box {
+                    Text(tag.name, modifier = Modifier.padding(8.dp))
+                    Text(
+                        "x",
+                        color = Color.Black,
+                        fontSize = 10.sp,
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(bottom = 12.dp, start = 4.dp, end = 2.dp)
+                    )
+                }
             }
         }
 
