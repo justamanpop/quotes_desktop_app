59eba97183d42c7bb5534772b05e669118f7b8bd
anish <pogchampoo24@gmail.com>
2025-11-05 19:40:04 +0530

    Added visible change to selected tags

--- composeApp/src/jvmMain/kotlin/org/example/quotes/shared/SelectedTags.kt
+++ composeApp/src/jvmMain/kotlin/org/example/quotes/shared/SelectedTags.kt
@@ -9,7 +9,12 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
@@ -19,14 +24,18 @@ import androidx.compose.ui.unit.dp
 fun SelectedTags(tags: Set<Tag>, removeTag: (Tag) -> Unit, modifier: Modifier = Modifier) {
     FlowRow(maxItemsInEachRow = 2, verticalArrangement = Arrangement.spacedBy(8.dp), modifier = modifier) {
         tags.forEach { tag ->
+            var isTagFocused by remember { mutableStateOf(false) }
             Card(
                 colors = CardColors(
-                    containerColor = Color.LightGray,
+                    containerColor = if (isTagFocused) Color.Gray else Color.LightGray,
                     contentColor = Color(64, 126, 201),
                     disabledContainerColor = Color.LightGray,
                     disabledContentColor = Color.White,
                 ),
-                modifier = Modifier.padding(start = 8.dp).pointerHoverIcon(PointerIcon.Hand)
+                modifier = Modifier.padding(start = 8.dp)
+                    .pointerHoverIcon(PointerIcon.Hand)
+                    .onFocusChanged { focusState -> isTagFocused = focusState.isFocused }
+                    .lightBorderIfFocused(isTagFocused, 2.dp)
                     .clickable(enabled = true, onClick = {
                         removeTag(tag)
                     })
