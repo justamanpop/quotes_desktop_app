08c13d14d4d2c89f9059be1c5a19ae505522522c
anish <pogchampoo24@gmail.com>
2025-11-17 16:25:11 +0530

    moved some functionality to commonMain

--- composeApp/src/jvmMain/kotlin/org/example/quotes/shared/utils.jvm.kt
+++ composeApp/src/jvmMain/kotlin/org/example/quotes/shared/utils.jvm.kt
@@ -1,43 +1,10 @@
 package org.example.quotes.shared
 
-import androidx.compose.foundation.border
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Transparent
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.isShiftPressed
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onPreviewKeyEvent
-import androidx.compose.ui.input.key.type
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import java.awt.Toolkit
 import java.awt.datatransfer.StringSelection
 import java.io.File
 
-fun Modifier.moveFocusOnTab() = composed {
-    val focusManager = LocalFocusManager.current
-    onPreviewKeyEvent {
-        if (it.type == KeyEventType.KeyDown && it.key == Key.Tab) {
-            focusManager.moveFocus(
-                if (it.isShiftPressed) FocusDirection.Previous else FocusDirection.Next
-            )
-            true
-        } else {
-            false
-        }
-    }
-}
-
-fun Modifier.lightBorderIfFocused(isFocused: Boolean, borderWidth: Dp = 1.dp) = composed {
-    Modifier.border(borderWidth, if (isFocused) Color.LightGray else Transparent)
-}
-
-fun copyToClipboard(text: String) {
+actual fun copyToClipboard(text: String) {
     val clipboard = Toolkit.getDefaultToolkit().systemClipboard
     val stringSelection = StringSelection(text)
     clipboard.setContents(stringSelection, null)
