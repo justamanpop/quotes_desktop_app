d0b4ba497e59de0bfd6403032cda43a9be8cf7c2
anish <pogchampoo24@gmail.com>
2025-11-19 17:28:57 +0530

    fixed delete modal button colors

--- composeApp/src/commonMain/kotlin/org/example/quotes/shared/DeleteConfirmationModal.kt
+++ composeApp/src/commonMain/kotlin/org/example/quotes/shared/DeleteConfirmationModal.kt
@@ -2,9 +2,11 @@ package org.example.quotes.shared
 
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,6 +23,12 @@ fun DeleteConfirmationModal(idToDelete: Int?, deleteFunction: (id: Int) -> Unit,
                     }
                     onDismissRequest()
                 },
+                colors = ButtonColors(
+                    contentColor = Color.White,
+                    containerColor = Color(201, 9, 35, 255),
+                    disabledContentColor = Color.White,
+                    disabledContainerColor = Color(186, 22, 39),
+                ),
             ) {
                 Text("Delete")
             }
@@ -28,6 +36,12 @@ fun DeleteConfirmationModal(idToDelete: Int?, deleteFunction: (id: Int) -> Unit,
         dismissButton = {
             Button(
                 onClick = onDismissRequest,
+                colors = ButtonColors(
+                    containerColor = Color(52, 161, 235),
+                    contentColor = Color.White,
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.Gray
+                ),
             ) {
                 Text("Cancel")
             }
