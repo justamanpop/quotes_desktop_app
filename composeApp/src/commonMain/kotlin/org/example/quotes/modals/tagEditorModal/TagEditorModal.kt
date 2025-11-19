6773edb0b44cc26dda760aa08253654099a8aea6
anish <pogchampoo24@gmail.com>
2025-11-19 23:24:30 +0530

    fixed edit tag modal button colors

--- composeApp/src/commonMain/kotlin/org/example/quotes/modals/tagEditorModal/TagEditorModal.kt
+++ composeApp/src/commonMain/kotlin/org/example/quotes/modals/tagEditorModal/TagEditorModal.kt
@@ -3,6 +3,7 @@ package org.example.quotes.modals.tagEditorModal
 import Tag
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,6 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -59,14 +61,30 @@ fun TagEditorModal(tagEditorMode: TagEditorMode, onDismissRequest: () -> Unit) {
                 )
 
                 Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-                    Button(onClick = {
-                        tagEditorMode.performAction(Tag(-1, tagNameTextFieldValue.text))
-                        onDismissRequest()
-                    }) {
+                    Button(
+                        onClick = {
+                            tagEditorMode.performAction(Tag(-1, tagNameTextFieldValue.text))
+                            onDismissRequest()
+                        },
+                        colors = ButtonColors(
+                            containerColor = Color(23, 176, 71),
+                            contentColor = Color.White,
+                            disabledContainerColor = Color(23, 176, 71),
+                            disabledContentColor = Color.Gray
+                        ),
+                    ) {
                         Text(tagEditorMode.getButtonText())
                     }
-                    Button(onClick = { onDismissRequest() }) {
-                        Text("Close")
+                    Button(
+                        onClick = { onDismissRequest() },
+                        colors = ButtonColors(
+                            contentColor = Color.White,
+                            containerColor = Color(201, 9, 35, 255),
+                            disabledContentColor = Color.White,
+                            disabledContainerColor = Color(186, 22, 39),
+                        ),
+                    ) {
+                        Text("Cancel")
                     }
                 }
             }
